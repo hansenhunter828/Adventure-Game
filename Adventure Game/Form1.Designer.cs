0138aaf2030e9349923bc24c5c7cffae2a4b69c5
Hunter Hansen <hunthans828@ed.amdsb.ca>
2021-02-25 09:21:25 -0500

    Add project files.

---
+++ Adventure Game/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Adventure_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.redLabel = new System.Windows.Forms.Label();
+            this.greenLabel = new System.Windows.Forms.Label();
+            this.yellowLabel = new System.Windows.Forms.Label();
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.brownLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // redLabel
+            // 
+            this.redLabel.AutoSize = true;
+            this.redLabel.Location = new System.Drawing.Point(593, 200);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(46, 17);
+            this.redLabel.TabIndex = 0;
+            this.redLabel.Text = "label1";
+            // 
+            // greenLabel
+            // 
+            this.greenLabel.AutoSize = true;
+            this.greenLabel.Location = new System.Drawing.Point(593, 317);
+            this.greenLabel.Name = "greenLabel";
+            this.greenLabel.Size = new System.Drawing.Size(46, 17);
+            this.greenLabel.TabIndex = 1;
+            this.greenLabel.Text = "label2";
+            // 
+            // yellowLabel
+            // 
+            this.yellowLabel.AutoSize = true;
+            this.yellowLabel.Location = new System.Drawing.Point(593, 269);
+            this.yellowLabel.Name = "yellowLabel";
+            this.yellowLabel.Size = new System.Drawing.Size(46, 17);
+            this.yellowLabel.TabIndex = 2;
+            this.yellowLabel.Text = "label3";
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.AutoSize = true;
+            this.blueLabel.Location = new System.Drawing.Point(593, 240);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(46, 17);
+            this.blueLabel.TabIndex = 3;
+            this.blueLabel.Text = "label4";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(46, 40);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(372, 17);
+            this.outputLabel.TabIndex = 4;
+            this.outputLabel.Text = "Welcome to my game. Please press the M key to continue";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(49, 158);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(538, 254);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // brownLabel
+            // 
+            this.brownLabel.AutoSize = true;
+            this.brownLabel.Location = new System.Drawing.Point(593, 360);
+            this.brownLabel.Name = "brownLabel";
+            this.brownLabel.Size = new System.Drawing.Size(46, 17);
+            this.brownLabel.TabIndex = 6;
+            this.brownLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(979, 450);
+            this.Controls.Add(this.brownLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.yellowLabel);
+            this.Controls.Add(this.greenLabel);
+            this.Controls.Add(this.redLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label greenLabel;
+        private System.Windows.Forms.Label yellowLabel;
+        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label brownLabel;
+    }
+}
+
---
+++ Adventure Game/Form1.cs
@@ -0,0 +1,407 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adventure_Game
+{
+    public partial class Form1 : Form
+    {
+        int scene = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.M) //Red Button Press
+            {
+                if (scene == 0) { scene = 1; }
+                else if (scene == 1) { scene = 2; }
+                else if (scene == 2 || scene == 3 || scene == 4 || scene == 5 || scene == 6) { scene = 8; }
+                else if (scene == 7) { scene = 99; }
+                else if (scene == 8) { scene = 10; }
+                else if (scene == 9) { scene = 11; }
+                else if (scene == 10) { scene = 11; }
+
+                else if (scene == 11) { scene = 12; }
+                else if (scene == 12) { scene = 13; }
+                else if (scene == 13) { scene = 15; }
+                else if (scene == 14) { scene = 16; }
+                else if (scene == 15) { scene = 18; }
+                else if (scene == 16) { scene = 18; }
+                else if (scene == 17) { scene = 16; }
+                else if (scene == 18) { scene = 20; }
+                else if (scene == 19) { scene = 99; }
+                else if (scene == 20) { scene = 99; }
+
+                else if (scene == 21) { scene = 23; }
+                else if (scene == 22) { }
+                else if (scene == 23) { scene = 24; }
+                else if (scene == 24)
+                {
+                    Random ranGen = new Random();
+                    int ranNum = ranGen.Next(1, 101);
+
+                    if (ranNum <= 15) { scene = 26; }
+                    else { scene = 30; }; 
+                }
+                else if (scene == 25) { scene = 99; }
+                else if (scene == 27) { scene = 28; }
+                else if (scene == 28) { scene = 27; }
+                else if (scene == 29) { scene = 27; }
+                else if (scene == 30) { scene = 32; }
+
+                else if (scene == 31) { scene = 32; }
+                else if (scene == 32) { scene = 33; }
+                else if (scene == 33) { }
+                else if (scene == 34) { scene = 99; }
+                else if (scene == 35) { }
+                else if (scene == 36) { }
+
+                else if (scene == 99) { scene = 0; }
+            }
+            else if (e.KeyCode == Keys.N)  //Yellow Button Press
+            {
+                if (scene == 0) { }
+                else if (scene == 1) { scene = 3; }
+                else if (scene == 2 || scene == 3 || scene == 4 || scene == 5 || scene == 6) { scene = 9; }
+                else if (scene == 9) { scene = 11; }
+            }
+            else if (e.KeyCode == Keys.B)  //Blue Button Press
+            {
+                if (scene == 0) { }
+                else if (scene == 1) { scene = 6; }
+                else if (scene == 2 || scene == 3 || scene == 4 || scene == 5 || scene == 6) { scene = 7; }
+                else if (scene == 8) { scene = 11; }
+
+                else if (scene == 13) { scene = 14; }
+                else if (scene == 14) { scene = 17; }
+                else if (scene == 18) { scene = 19; }
+                else if (scene == 19) { scene = 99; }
+
+                else if (scene == 21) { scene = 22; }
+                else if (scene == 22) { scene = 99; }
+                else if (scene == 23) { scene = 37; }
+                else if (scene == 24) { scene = 25; }
+                else if (scene == 25) { scene = 99; }
+                else if (scene == 26) { }
+                else if (scene == 27) { scene = 29; }
+                else if (scene == 28) { }
+                else if (scene == 29) { scene = 30; }
+                else if (scene == 30) { }
+
+                else if (scene == 31) { }
+                else if (scene == 32) { scene = 34; }
+                else if (scene == 33) { }
+                else if (scene == 34) { }
+                else if (scene == 35) { }
+                else if (scene == 36) { }
+
+                else if (scene == 99) { scene = 100; }
+            }
+            else if (e.KeyCode == Keys.Space) //Green Button Press
+            {
+                if (scene == 0) { }
+                else if (scene == 1) { scene = 5; }
+            }
+            else if (e.KeyCode == Keys.V)  //Brown Button Press
+            {
+                if (scene == 0) { }
+                else if (scene == 1) { scene = 4; }
+            }
+
+            switch (scene) 
+            {
+                case 0:
+                    break;
+                case 1:
+                    outputLabel.Text = "Hey who are you? Your not on the list says a guard.";
+                    outputLabel.Text += "\nWhat species are you?";
+
+                    redLabel.Text = "Dark Elf";
+                    blueLabel.Text = "Orc";
+                    yellowLabel.Text = "Argonian";
+                    greenLabel.Text = "Khajiit";
+                    brownLabel.Text = "Nord";
+                    break;
+                case 2:
+                    outputLabel.Text = "You better not use any magic to get out of this you witch says the guard. What do you do?";
+
+                    redLabel.Text = "My my, you are looking so good today kind sir.(Compliment)";
+                    blueLabel.Text = "Shut up! Your a no good nord!(Insult)";
+                    yellowLabel.Text = "(Remain Silent)";
+                    greenLabel.Text = "";
+                    brownLabel.Text = "";
+                    break;
+                case 3:
+                    outputLabel.Text = "Argonian ey? what are you doing so far from the waters?";
+
+                    redLabel.Text = "My my, you are looking so good today kind sir.(Compliment)";
+                    blueLabel.Text = "Shut up! Your a no good nord!(Insult)";
+                    yellowLabel.Text = "(Remain Silent)";
+                    greenLabel.Text = "";
+                    brownLabel.Text = "";
+                    break;
+                case 4:
+                    outputLabel.Text = "Im sorry brother you have to go this way";
+
+                    redLabel.Text = "My my, you are looking so good today kind sir.(Compliment)";
+                    blueLabel.Text = "Shut up! Your a no good nord!(Insult)";
+                    yellowLabel.Text = "(Remain Silent)";
+                    greenLabel.Text = "";
+                    brownLabel.Text = "";
+                    break;
+                case 5:
+                    outputLabel.Text = "Your a khajit you thief! you deserve this";
+
+                    redLabel.Text = "My my, you are looking so good today kind sir.(Compliment)";
+                    blueLabel.Text = "Shut up! Your a no good nord!(Insult)";
+                    yellowLabel.Text = "(Remain Silent)";
+                    greenLabel.Text = "";
+                    brownLabel.Text = "";
+                    break;
+                case 6:
+                    outputLabel.Text = "An orc? weve been at war with your people for years. Your one less to deal with now says the guard. What do you do?";
+
+                    redLabel.Text = "My my, you are looking so good today kind sir.(Compliment)";
+                    blueLabel.Text = "Shut up! Your a no good nord!(Insult)";
+                    yellowLabel.Text = "(Remain Silent)";
+                    greenLabel.Text = "";
+                    brownLabel.Text = "";
+                    break;
+                case 7:
+                    outputLabel.Text = "Haha very  funny your first now.";
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    break;
+                case 8:
+                    outputLabel.Text = "Thanks but your still on the list. Get in line.";
+
+                    redLabel.Text = "Please no! I dont want to die.(Beg)";
+                    blueLabel.Text = "(Remain Silent)";
+                    break;
+                case 9:
+                    outputLabel.Text = "Your a quite one ey?(Press M to continue)";
+                    
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    break;
+                case 10:
+                    outputLabel.Text = "Save it for the block the guards tells you.";
+
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    break;
+                case 11:
+                    outputLabel.Text = "An orc army attacks the compound. The guards get distracted and give you time to escape. What do you do?";
+
+                    redLabel.Text = "Join the nords";
+                    blueLabel.Text = "Join the orcs";
+                    break;
+                case 12:
+                    outputLabel.Text = "Look I know you were about to be executed but Ill pardon you if you aid us says a guard. What do you do?";
+
+                    redLabel.Text = "Yes I will help you defeat the orcs.(Accept Pardon)";
+                    blueLabel.Text = "No I will never accept your pardon(Refuse Pardon)";
+                    break;
+                case 13:
+                    outputLabel.Text = "Thank you Ill make sure my higher ups know what you have done. We must hurry to aid the men at the front gate";
+                    outputLabel.Text += " You arrive at the front gate. Do you help the men on the wall or do you aid the men on the ground?";
+
+                    redLabel.Text = "Help them men ground";
+                    blueLabel.Text = "Help the men on the wall";
+                    break;
+                case 14:
+                    outputLabel.Text = "A soldier hands you a bow and tells you to start shooting the orcs. Do you take time to aim or fire as fast as you can";
+
+                    redLabel.Text = "Hit your target";
+                    blueLabel.Text = "Fire willy nilly";
+                    break;
+                case 15:
+                    outputLabel.Text = "You run to help the soldiers on the ground and after tons of hacking and slashing the orc army finally retreats";
+                    outputLabel.Text = "Press M to continue";
+
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    break;
+                case 16:
+                    outputLabel.Text = "You take time to aim and you manage to fend of the orc attack(press M to continue)";
+
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    break;
+                case 17:
+                    outputLabel.Text = "HEY take your time to aim a soldier yells at you(press M to continue)";
+
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    break;
+                case 18:
+                    outputLabel.Text = "The soldier that promised you pardon walks up to you and says good job. Now i can offer you two things.";
+                    outputLabel.Text += "The first one is the pardon I promised you and the other one is the pardon and an offer to join our army.";
+                    outputLabel.Text += "What do you do?";
+
+                    redLabel.Text = "I would never join your army!(Dont Join)";
+                    blueLabel.Text = "Yes I would be happy to join your army!(Join Nords)";
+                    break;
+                case 19:
+                    outputLabel.Text = "Thats great to hear but I lied I can never let a criminal scum like you live he says as you feel something cold plunge into you stomach as you fall over and die";
+                    outputLabel.Text += "Press M to continue";
+
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    break;
+                case 20:
+                    outputLabel.Text = "well that sucks, well Im a man of my word you have been pardoned just make sure you dont do anything else bad";
+                    outputLabel.Text += "Or we will be meeting again";
+
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    break;
+                case 21:
+                    outputLabel.Text = "You see an orc brutally cut down the executioner down and then he says to you are you friend or foe?";
+
+                    redLabel.Text = "I am on your side(Friend)";
+                    blueLabel.Text = "I am not your friend! In fact I am your worst nigtmare(Foe)";
+                    break;
+                case 22:
+                    outputLabel.Text = "The orc takes his axe chops your head off(Press M to continue)";
+
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    break;
+                case 23:
+                    outputLabel.Text = "Good he says he hands you a spare battle axe and tells you to aid them in defeating the nords. What do you do?";
+
+                    redLabel.Text = "Fight the nords";
+                    blueLabel.Text = "Run away";
+                    break;
+                case 37:
+                    outputLabel.Text = "As you start running away you feel a sharp pain in your back as you fall to the ground and die";
+                    break;
+                case 24:
+                    outputLabel.Text = "You fight with the orcs hacking and slashing at every nord you see until the battle is won.";
+                    outputLabel.Text = "You fight pretty well. Would you like to join our army?";
+
+                    redLabel.Text = "Yes I would love to!(Join Army)";
+                    blueLabel.Text = "No I would hate to.(Dont Join)";
+                    break;
+                case 25:
+                    outputLabel.Text = "Ah well ok then but you really are a great fighter.";
+                    outputLabel.Text += "I wish you well and if you ever want to join we would be happy to recruit you(Press M to continue)";
+
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    break;
+                case 26:
+                    outputLabel.Text = "Thats great hear lets get you some armor for out next raid on the next nord compound";
+                    outputLabel.Text += "You get your armor and you head off for the next battle do you small talk with some orcs or stay quite?";
+
+                    redLabel.Text = "Small talk with some orcs";
+                    blueLabel.Text = "Dont talk to the orcs";
+                    break;
+                case 27:
+                    outputLabel.Text = "You party gets ambushed by wolves do you attack them or tend the wounded?";
+
+                    redLabel.Text = "Attack the wolves";
+                    blueLabel.Text = "Tend the wounded";
+                    break;
+                case 28:
+                    outputLabel.Text = "You attack and slaughter the pack of wolves and let the wounded orc bleed out and die.(Press M to continue";
+
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    yellowLabel.Text = "";
+                    break;
+                case 29:
+                    outputLabel.Text = "You tend the wounded orc and he thanks you.(Press M to continue)";
+
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    break;
+                case 30:
+                    outputLabel.Text = "You try to talk to the orcs but none of them seem to want to talk so you spend the travel alone.(Press M to continue)";
+
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    break;
+                case 31:
+                    outputLabel.Text = "You stay quite the entire travel(Press M to continue";
+
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    break;
+                case 32:
+                    outputLabel.Text = "You see the compound on the horizon and all of the orcs start charging. Do you charge with them or stay behind?";
+
+                    redLabel.Text = "I charge at the front gate full speed";
+                    blueLabel.Text = "I stay behind and let the orcs go first";
+                    break;
+                case 33:
+                    outputLabel.Text = "As you charge at the compound you see the air fill with arrows and you see your allies falling until your the last one alive";
+                    outputLabel.Text += "\nYou see nords surrounding you do you fight them or surrender?";
+
+                    redLabel.Text = "I surrender please dont hurt me";
+                    blueLabel.Text = "I will never surrender";
+                    break;
+                case 34:
+                    outputLabel.Text = "as you stay behind and watch the charge you hear a growl and as you turn around you see a hungry saber cat.(Press M to continue)";
+
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    break;
+                case 35:
+                    outputLabel.Text = "you surrender to the nords and notice that you are in the same position you were when the orcs saved you but";
+                    outputLabel.Text += "\nthis time you know no one will save you. The executioner chops your head off and you die(Press M to continue)";
+
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    break;
+                case 36:
+                    outputLabel.Text = "You yell I will never surrender as you get hit with arrow after arrow after arrow and you die(Press M to continue)";
+
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    break;
+                case 99:
+                    outputLabel.Text = "Thank you for playing! Do You want to play again?";
+
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 100:
+                    Application.Exit();
+                    break;
+
+
+
+
+            }
+        }
+
+    }
+}
